9541b9d8e63eb6d02630113a662f282f8ebafad0
Kei Misawa <7822535+330k@users.noreply.github.com>
2023-02-23 19:32:34 +0900

    wgetのリトライ回数を最大100回に制限

--- gather_weather_data.sh
+++ gather_weather_data.sh
@@ -29,7 +29,7 @@ for i in $POINTS; do
   DIST=$(echo $i | cut -d',' -f4)
 
   URL="https://api.open-meteo.com/v1/forecast?latitude=${LAT}&longitude=${LON}&hourly=temperature_2m,precipitation,rain,showers,snowfall,weathercode,windspeed_10m,winddirection_10m&windspeed_unit=ms&start_date=${STARTDATE}&end_date=${ENDDATE}"
-  while true; do
+  for((j=0;j<100;j++)); do
     wget "${URL}" --no-check-certificate --timeout 10 -t 30 -nv -O ${WORKDIR}/${NAME}_weather.json && break
   done
   echo '{"name":"'${NAME}'","dist":'${DIST}'}' > ${WORKDIR}/${NAME}_add.json
